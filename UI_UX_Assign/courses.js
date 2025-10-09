255afb388d82c336aec1613ceebee87fb390b15a
Alfredio <caleb.j.heino@gmail.com>
2025-10-09 10:23:04 -0400

    Added our product backlog assignment

--- UI_UX_Assign/courses.js
+++ UI_UX_Assign/courses.js
@@ -2,6 +2,8 @@
 const courses = [
   { number: "INFO450", department: "INFO", instructor: "Dr. McGarry", credits: 3, prereq: "INFO300", modality: "Hybrid",   max: 30, enrolled: 0 },
   { number: "SCMA320", department: "SCMA", instructor: "Prof. Johnson", credits: 3, prereq: "MATH200",  modality: "In-person", max: 40, enrolled: 0 },
+  { number: "MGMT303", department: "MGMT", instructor: "Prof. Ruiz", credits: 3, prereq: "MGMT210",  modality: "In-person", max: 40, enrolled: 0 },
+  { number: "ACCT204", department: "ACCT", instructor: "Dr. Krumwiedie", credits: 3, prereq: "ACT203",  modality: "In-person", max: 40, enrolled: 0 },
   { number: "MATH200", department: "MATH", instructor: "Dr. Lee",     credits: 4, prereq: "None",     modality: "Hybrid",   max: 35, enrolled: 0 }
 ];
 
